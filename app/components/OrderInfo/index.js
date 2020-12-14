722655a67663333bf68fad3edaf96925b3761a58
Bui Le Huynh <huynh.bui@tienve.com.vn>
2020-12-14 16:50:44 +0700

    + Order Info Component: change OrderId Positions

--- app/components/OrderInfo/index.js
+++ app/components/OrderInfo/index.js
@@ -23,37 +23,29 @@ function OrderInfo(props) {
     <Card>
       <CardContent>
         <Grid container spacing={1}>
-          {/* Title Ma don yeu cau */}
+          {/* Ma don yeu cau */}
           <Grid item xs={6}>
-            <Typography color="textSecondary" gutterBottom>
+            <Typography color="body2" component="p">
               {' '}
               Mã đơn yêu cầu{' '}
             </Typography>
           </Grid>
 
           <Grid item xs={6}>
-            <Typography color="textSecondary" align="right" gutterBottom />
-          </Grid>
-          <Divider />
-
-          {/* Ma don yeu cau */}
-          <Grid item xs={6}>
-            <Typography variant="h5" component="h2">
-              {props.orderId}
+            <Typography color="body2" align="right" component="p">
+              <b>{props.orderId}</b>
             </Typography>
           </Grid>
-
-          <Grid item xs={6} />
           <Divider />
 
           {/* Trang thai */}
           <Grid item xs={6}>
-            <Typography color="textSecondary"> Trạng thái </Typography>
+            <Typography color="body2"> Trạng thái </Typography>
           </Grid>
 
           <Grid item xs={6}>
             <Typography align="right" color="textSecondary">
-              {props.orderStatus}
+              <b>{props.orderStatus}</b>
             </Typography>
           </Grid>
           <Divider />
